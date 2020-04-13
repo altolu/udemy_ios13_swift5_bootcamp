3c638a45ea8c488c0af7ed14ca4ac10bcc5d6675
altolu.llc@gmail.com <altolu.llc@gmail.com>
2020-04-13 11:41:14 -0700

    SwiftUI app KasiaCard

---
+++ KasiaCard/KasiaCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  KasiaCard
+//
+//  Created by Kasia Hayden on 4/13/20.
+//  Copyright © 2020 Altolu. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName).foregroundColor(.green)
+                Text(text).foregroundColor(.black)
+                }
+        )
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
